e5739ce88f5aa7892eb12e97be7a0827e18226a4
Andrius Valauskas <andriusv1993@gmail.com>
2022-11-15 20:07:04 +0200

    flaskintro

--- main.py
+++ main.py
@@ -7,3 +7,14 @@
 # -render_template
 # -exted tempalte
 
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET', 'POST'])         #decorator
+def hello_world():
+    print(request.method)
+    print(request.form.get('username'))
+    return render_template('form.html')
+
+app.run()
